24bbdef7c500575d0277b3cbda1073cdbade6119
marqh <markh@metarelate.net>
2016-05-20 13:38:32 +0000

    test pattern

--- lib/cube_browser/tests/unit/test_Contour.py
+++ lib/cube_browser/tests/unit/test_Contour.py
@@ -1,6 +1,6 @@
 from __future__ import (absolute_import, division, print_function)
 from six.moves import (filter, input, map, range, zip)  # noqa
-"""Unit tests for the `cube_browser.Contourf` class."""
+"""Unit tests for the `cube_browser.Contour` class."""
 
 # Import iris.tests first so that some things can be initialised
 # before importing anything else.
@@ -10,7 +10,7 @@ from iris.tests.stock import realistic_3d
 
 from cartopy.mpl.geoaxes import GeoAxesSubplot
 from matplotlib.contour import QuadContourSet
-from cube_browser import Contourf
+from cube_browser import Contour
 
 
 class Test__call__(tests.IrisTest):
@@ -20,7 +20,7 @@ class Test__call__(tests.IrisTest):
                         'grid_latitude')  # plot axis coordinates.
 
     def test_plot_type(self):
-        cf = Contourf(self.cube, self.pcoords)
+        cf = Contour(self.cube, self.pcoords)
         ax = cf(time=0)
         self.assertTrue(isinstance(ax, GeoAxesSubplot))
         self.assertTrue(isinstance(cf.qcs, QuadContourSet))
